7a5e5a1ff01dd7951d6cd3a12aa505c8bd5c38c7
edcela <abanto.edcel@gmail.com>
2025-10-30 17:15:50 -0500

    Made diff bot default, will look into a better way of launching view_robot for both rover and arm.

--- umrt-project-perry-description/launch/view_robot.launch.py
+++ umrt-project-perry-description/launch/view_robot.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="project_perry.urdf.xacro",
+            default_value="diffbot.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
@@ -79,7 +79,7 @@ def generate_launch_description():
     robot_description = {"robot_description": robot_description_content}
 
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "rviz", "project_perry_view.rviz"]
+        [FindPackageShare(description_package), "rviz", "diffbot_view.rviz"]
     )
 
     joint_state_publisher_node = Node(
